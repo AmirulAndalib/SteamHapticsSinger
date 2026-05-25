2cfc907d15301d7a8faab0b86bf0521a36797f47
CrazyCritic89 <cxrx5000@gmail.com>
2026-05-24 22:24:56 -0500

    Some beginnings for gain curve

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@
 #define DURATION_MAX		-1
 #define NOTE_STOP		   -1
 
-#define DEFAULT_GAIN 127
+#define DEFAULT_GAIN 0
 
 #define VALVE_VID	 			0x28DE
 #define STEAM_CONTROLLER 		0x1101
@@ -33,7 +33,7 @@
 using namespace std;
 
 double midiFrequency[128]  = {0, 8.66196, 9.17702, 9.72272, 10.3009, 10.9134, 11.5623, 12.2499, 12.9783, 13.75, 14.5676, 15.4339, 16.3516, 17.3239, 18.354, 19.4454, 20.6017, 21.8268, 23.1247, 24.4997, 25.9565, 27.5, 29.1352, 30.8677, 32.7032, 34.6478, 36.7081, 38.8909, 41.2034, 43.6535, 46.2493, 48.9994, 51.9131, 55, 58.2705, 61.7354, 65.4064, 69.2957, 73.4162, 77.7817, 82.4069, 87.3071, 92.4986, 97.9989, 103.826, 110, 116.541, 123.471, 130.813, 138.591, 146.832, 155.563, 164.814, 174.614, 184.997, 195.998, 207.652, 220, 233.082, 246.942, 261.626, 277.183, 293.665, 311.127, 329.628, 349.228, 369.994, 391.995, 415.305, 440, 466.164, 493.883, 523.251, 554.365, 587.33, 622.254, 659.255, 698.456, 739.989, 783.991, 830.609, 880, 932.328, 987.767, 1046.5, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760, 1864.66, 1975.53, 2093, 2217.46, 2349.32, 2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520, 3729.31, 3951.07, 4186.01, 4434.92, 4698.64, 4978.03, 5274.04, 5587.65, 5919.91, 6271.93, 6644.88, 7040, 7458.62, 7902.13, 8372.02, 8869.84, 9397.27, 9956.06, 10548.1, 11175.3, 11839.8, 12543.9};
-
+uint8_t gainCurve[128] = {DEFAULT_GAIN};
 
 struct ParamsStruct{
 	const char* midiSong;
@@ -62,7 +62,7 @@ enum class ControllerType {
 struct SteamControllerInfos{
 	libusb_device_handle* dev_handle = NULL;
 	hid_device* hid_handle = NULL;
-	int interfaceNum = NULL;
+	int interfaceNum = 0;
 	ControllerType type = ControllerType::None;
 };
 
@@ -79,7 +79,7 @@ hid_device* open_steam_controller_hid(uint16_t pid) {
 			handle = hid_open_path(cur->path);
 			if (handle) {
 				//Check if any data being sent, if valid controller, will always be sending
-				r = hid_read_timeout(handle,buf,sizeof(buf),100);
+				r = hid_read_timeout(handle,buf,64,100);
 				if (r > 0) break;
 			}
 		}
@@ -412,7 +412,7 @@ void playSong(SteamControllerInfos* controller,const ParamsStruct params){
 
 bool parseArguments(int argc, char** argv, ParamsStruct* params){
 	int c;
-	while ( (c = getopt(argc, argv, "l:r:n:m:d:i:pets")) != -1) {
+	while ( (c = getopt(argc, argv, "l:r:n:m:d:i:pvuts")) != -1) {
 		int32_t value;
 		switch(c){
 		case 'l':
@@ -454,8 +454,11 @@ bool parseArguments(int argc, char** argv, ParamsStruct* params){
 		case 'p':
 			params->repeatSong = true;
 			break;
-		case 'e':
+		case 'v':
 			directVel = true;
+			break;
+		case 'u':
+			memset(gainCurve, DEFAULT_GAIN, 128);
 			break;
 		case 't':
 			tritonLimit = true;
@@ -522,7 +525,8 @@ int main(int argc, char** argv)
 			  "\n  -i INTERVAL		Player sleep interval (in microseconds). Lower generally means better song fidelity, but higher cpu usage, and at some point going lower won't improve any more. Default value is 10000"
 			  "\n  -d DEBUG_LEVEL	Libusb debug level. Default is 0, no debug output. max is 4, max verbosity output"
 		      "\n  -p	Repeat song, plays again after ending"
-			  "\n  -e 	Direct velocity to gain control, the MIDI file will set the gain"
+			  "\n  -v 	Direct velocity to gain control, the MIDI file will set the gain"
+			  "\n  -u	No gain curve, all notes use (signed) 0x00 gain"
 			  "\n  -t	(Steam Controller 2026 Only) Limit to only two channels"
 			  "\n  -s	(Steam Controller 2026 Only) Swap rumble and trackpad channels"
 				"" << endl;
